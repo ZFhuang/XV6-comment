4d3316d130422ee9df4fa0657a026d7ff1c6eedb
Zifeng <huangsnake@gmail.com>
2019-06-16 20:25:49 +0800

    可以正常读取和打印内容了

--- XV6-code/editor.c
+++ XV6-code/editor.c
@@ -7,20 +7,21 @@
 // ************************************************************ const strings *
 #define MAX_CHAR_INSHOW 80
 #define BUF_LINE_LENGTH 320
+#define NULL 0
 
 // *********************************************************** data structure *
 // use linkedlist to store the text
 typedef struct LineNode {
 	struct LineNode* prev;
-	struct LineNode* next;
 	int length;
 	char* content;
+	struct LineNode* next;
 }LineNode;
 
 // **************************************************** function decalrations *
 int loadFile(char* filePath);
 void showFile();
-void initList();
+int initList();
 LineNode* newNode(LineNode* prev);
 char* setStr(char* target, char* from, int len);
 
@@ -41,10 +42,11 @@ int main(int argc, char* argv[])
 
 	if (loadFile(argv[1])) {
 		// check successfully
-		printf(1, "File lines: %d", fileLineNum);
+		printf(1, "Load successfully! File lines: %d\n", fileLineNum);
 	}
 	else {
 		// something goes wrong
+		printf(1, "Load unsuccessfully!\n\n");
 		exit();
 		return -1;
 	}
@@ -67,19 +69,22 @@ int loadFile(char* filePath) {
 	printf(1, "File found!\n");
 
 	// init it
-	initList();
+	if (!initList()) {
+		printf(1, "initList error!\n");
+		return 0;
+	}
 
 	// the most important part
 	// adding every lines into that linkedlist
 	char buf[BUF_LINE_LENGTH] = {};
 	int len = 0;
-	int start = 0;
-	int end = 0;
 	int scaning = 0;
-	LineNode* now;
+	LineNode* now = NULL;
 
 	// read a new line
-	while ((len = read(file, buf, BUF_LINE_LENGTH))) {
+	while ((len = read(file, buf, BUF_LINE_LENGTH)) > 0) {
+		int start = 0;
+		int end = 0;
 		// if the [start pointer] is still inline
 		while (start < len) {
 			// find out the line's ending
@@ -91,9 +96,14 @@ int loadFile(char* filePath) {
 				now = newNode(tailNode->prev);
 			}
 
+			if (now == NULL) {
+				printf(1, "nowNode pointer is NULL!\n");
+				return 0;
+			}
+
 			// if is a new line
 			if (now->length == 0) {
-				now->content = (char*)malloc(sizeof(char)*now->length);
+				now->content = (char*)malloc(sizeof(char)*(0 + end - start));
 				setStr(&now->content[0], &buf[start], end - start);
 				now->length = 0 + end - start;
 			}
@@ -110,6 +120,7 @@ int loadFile(char* filePath) {
 			// if meets a line's ending
 			if (buf[end] == '\n') {
 				scaning = 0;
+				now = NULL;
 				fileLineNum++;
 			}
 			// this line is not end now
@@ -125,26 +136,53 @@ int loadFile(char* filePath) {
 	return 1;
 }
 
-// display file to commandline
+// display file in commandline
 void showFile() {
-	;
+	LineNode* now = headNode->next;
+	while (now!=tailNode)
+	{
+		// display one line
+		printf(1, "%s\n",now->content);
+		now = now->next;
+	}
 }
 
 // init linkedlist
-void initList() {
+int initList() {
 	tailNode = (LineNode*)malloc(sizeof(LineNode));
 	headNode = (LineNode*)malloc(sizeof(LineNode));
+	if (tailNode == 0 || headNode == 0) {
+		printf(1, "malloc error!\n");
+		return 0;
+	}
 
 	tailNode->prev = headNode;
-	tailNode->next = 0;
-	headNode->prev = 0;
+	tailNode->content = NULL;
+	tailNode->length = 0;
+	tailNode->next = NULL;
+
+	headNode->prev = NULL;
+	headNode->content = NULL;
+	headNode->length = 0;
 	headNode->next = tailNode;
+
+	return 1;
 }
 
 // add a new node after [prev] and return it
 LineNode* newNode(LineNode* prev) {
+	if (prev == NULL) {
+		printf(1, "the newNode's prev pointer is NULL!\n");
+		return NULL;
+	}
 	LineNode* node;
 	node = (LineNode*)malloc(sizeof(LineNode));
+
+	if (node == NULL) {
+		printf(1, "newNode alloc failed!\n");
+		return NULL;
+	}
+
 	LineNode* next = prev->next;
 	if (next) {
 		next->prev = node;
@@ -155,8 +193,10 @@ LineNode* newNode(LineNode* prev) {
 	else {
 		prev->next = node;
 		node->prev = prev;
-		node->next = 0;
+		node->next = NULL;
 	}
+	node->content = NULL;
+	node->length = 0;
 
 	return node;
 }
